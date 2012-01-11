f4a9076b1f503693a389492ff7a80f1930efe0fd
Filipe Cabecinhas <filcab+github@gmail.com>
2012-01-11 15:22:06 +0000

    New strike detail view implementation.

---
+++ Hagreve Mobile/HGStrikeDetailViewController.h
@@ -0,0 +1,39 @@
+//
+//  HGStrikeDetailViewController.h
+//  Hagreve Mobile
+//
+//  Created by Filipe Cabecinhas on 2012/01/09.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HGStrike.h"
+
+#import <UIKit/UIKit.h>
+
+@interface HGStrikeDetailViewController : UIViewController <UIActionSheetDelegate>
+
+@property (nonatomic, retain) HGStrike *strike;
+
+@property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
+
+@property (nonatomic, retain) IBOutlet UILabel *startDateLabel;
+@property (nonatomic, retain) IBOutlet UILabel *endDateLabel;
+@property (nonatomic, retain) IBOutlet UILabel *companyLabel;
+@property (nonatomic, retain) IBOutlet UILabel *commentLabel;
+@property (nonatomic, retain) IBOutlet UIButton *sourceButton;
+@property (nonatomic, retain) IBOutlet UIButton *tweetButton;
+
+- (IBAction)sourceTouch:(id)sender;
+- (IBAction)tweetTouch:(id)sender;
+
+#pragma mark - Date formatters
+- (NSString*)labelForDate:(NSDate*)date allDay:(BOOL)all_day;
+
+#pragma mark - UI stuff
+- (UIFont*)commentFont;
+#define BORDER 4
+#define WIDTH 320
+#define MIN_HEIGHT 29
+#define REGULAR_FONT_SIZE 17.0
+
+@end
---
+++ Hagreve Mobile/HGStrikeDetailViewController.m
@@ -0,0 +1,159 @@
+//
+//  HGStrikeDetailViewController.m
+//  Hagreve Mobile
+//
+//  Created by Filipe Cabecinhas on 2012/01/09.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HGStrikeDetailViewController.h"
+
+@implementation HGStrikeDetailViewController
+
+@synthesize strike         =         _strike,
+            scrollView     =     _scrollView,
+            startDateLabel = _startDateLabel,
+            endDateLabel   =   _endDateLabel,
+            companyLabel   =   _companyLabel,
+            commentLabel   =   _commentLabel,
+            sourceButton   =   _sourceButton,
+            tweetButton    =    _tweetButton;
+
+#pragma mark - Button actions
+
+- (IBAction)tweetTouch:(id)sender {
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Tweeting…" message:@"Lol, you wish!" delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles: nil];
+    
+    [alertView show];
+}
+
+
+- (IBAction)sourceTouch:(id)sender {
+    UIActionSheet *sheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Cancelar" destructiveButtonTitle:nil otherButtonTitles:@"Copiar", @"Abrir", nil];
+    sheet.actionSheetStyle = UIActionSheetStyleAutomatic;
+    [sheet showInView:self.view];
+}
+
+#pragma mark - UIActionSheet delegate
+
+#define SHEET_COPY_IDX 0
+#define SHEET_OPEN_IDX 1
+#define SHEET_CANCEL_IDX 2
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    switch (buttonIndex) {
+        case SHEET_COPY_IDX: {
+            UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+            [pasteboard setURL:self.strike.sourceLink];
+            break;
+        }
+        case SHEET_OPEN_IDX:
+            [[UIApplication sharedApplication] openURL:self.strike.sourceLink];
+            break;
+        case SHEET_CANCEL_IDX:
+            // No-op
+            break;
+    }
+}
+
+#pragma mark - View
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewWillAppear:(BOOL)animated {
+    // Setup the UILabels for the strike information.
+    self.startDateLabel.text = [self labelForDate:self.strike.startDate allDay:self.strike.all_day];
+    self.endDateLabel.text   = [self labelForDate:self.strike.endDate allDay:self.strike.all_day];
+    self.companyLabel.text   = self.strike.company.name;
+
+    if (self.strike.canceled){
+        // Mark the strike as canceled.
+        UIImage *canceledImage = [UIImage imageNamed:@"Canceled"];
+        UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, (31+33)+29, 320, 70)];
+        NSLog(@"%@\n\n%@\n\n\n\n\n", canceledImage, imageView);
+        [imageView setImage:canceledImage];
+        [self.scrollView addSubview:imageView];
+    }
+
+    CGRect frame = self.commentLabel.frame;
+    CGSize textSize = {0, 0};
+    NSString *text = self.strike.comment;
+    if (text && ![text isEqualToString:@""])
+        textSize = [text sizeWithFont:[self commentFont]
+                    constrainedToSize:CGSizeMake(WIDTH, 42000)
+                        lineBreakMode:UILineBreakModeWordWrap];
+    CGFloat height = MIN_HEIGHT < textSize.height ? textSize.height + BORDER : MIN_HEIGHT;
+
+    self.commentLabel.frame = CGRectMake(CGRectGetMinX(frame), CGRectGetMinY(frame), CGRectGetWidth(frame), height);
+    self.commentLabel.text   = self.strike.comment;
+}
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Date formatters
+- (NSString*)labelForDate:(NSDate*)date allDay:(BOOL)all_day {
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+
+    if (all_day)
+        [dateFormatter setTimeStyle:NSDateFormatterNoStyle];
+    else
+        [dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+
+    return [dateFormatter stringFromDate:date];
+}
+
+#pragma mark - UI stuff
+
+- (UIFont *)commentFont {
+    static UIFont *font;
+    if (nil == font)
+        font = [UIFont boldSystemFontOfSize:REGULAR_FONT_SIZE];
+    return font;
+}
+
+
+@end
