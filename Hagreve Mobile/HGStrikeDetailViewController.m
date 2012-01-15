076d76ba678c672461b8336ae1f0507d6d60638e
Filipe Cabecinhas <filcab+github@gmail.com>
2012-01-15 16:52:20 +0000

    Support upside down orientation in the StrikeDetailViewController.

--- Hagreve Mobile/HGStrikeDetailViewController.m
+++ Hagreve Mobile/HGStrikeDetailViewController.m
@@ -157,7 +157,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return (interfaceOrientation == UIInterfaceOrientationPortrait) || (interfaceOrientation == UIInterfaceOrientationPortraitUpsideDown);
 }
 
 #pragma mark - Date formatters
